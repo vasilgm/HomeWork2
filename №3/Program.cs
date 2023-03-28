6be2b0d6e4ad2aa50a91790305f04a8bbd2d5ed0
Vasil <vasilgm@yandex.ru>
2023-03-28 19:51:35 +0300

    №3

---
+++ №3/Program.cs
@@ -0,0 +1,25 @@
+﻿Day ();
+
+void Day ()
+{
+System.Console.WriteLine("Введите день недели:");
+int N = int.Parse(Console.ReadLine());
+
+if (7 >= N & N >= 1)
+{
+    if( N == 6 | N == 7)
+    {
+    System.Console.WriteLine("Выходной день!");
+    }
+    else
+    {
+    System.Console.WriteLine("Рабочий день!");
+    }
+}
+
+else 
+{
+    System.Console.WriteLine("Неверно введен день недели!");
+
+}
+}
