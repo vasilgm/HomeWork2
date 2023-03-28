d8ef04095b66425913dd992ce1e6e39f79e0d14e
Vasil <vasilgm@yandex.ru>
2023-03-28 18:57:42 +0300

    №1 and №2

---
+++ №1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int N = int.Parse(Console.ReadLine());
+
+if ( 1000 > N && N > 99)
+{
+    Console.WriteLine($"Вторая цифра числа равна {Two(N)}");
+}
+
+else 
+{
+    Console.WriteLine($"Число {N} не трехзначное!!!");
+}
+
+int Two (int N)
+{
+    N = N/10;
+    return N = N%10;
+}
---
+++ №2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число N:");
+int N = int.Parse(Console.ReadLine());
+if (N >=100)
+{
+N = threenumber(N);
+Console.WriteLine($"Третья цифра числа равна {remainder (N)}");
+}
+else
+{
+    System.Console.WriteLine("Третьей цифры нет!");
+}
+
+int threenumber (int number)
+{
+while (number > 999)
+{
+number = number / 10;
+}
+return number;
+}
+
+int remainder (int number)
+{
+    number = number % 10;
+    return number;
+}
